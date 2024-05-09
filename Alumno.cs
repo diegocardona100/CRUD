7cebecfe60a9258419ff69392e841095b2417990
diegocardona100 <diego_sac@umes.edu.gt>
2024-05-09 09:31:12 -0600

    Agregar archivos de proyecto.

---
+++ Alumno.cs
@@ -0,0 +1,15 @@
+﻿namespace FormValidacion
+{
+    public class Alumno : Persona
+    {
+        public string NumeroCarne { get; set; }
+        public List<int> Notas { get; set; }
+
+        public Alumno()
+        {
+            NumeroCarne = string.Empty;
+            Notas = new List<int>();
+        }
+
+    }
+}
---
+++ Catedraticos.cs
@@ -0,0 +1,21 @@
+﻿using FormValidacion;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppFormulariosValidacion
+{
+    public class Catedratico : Persona
+    {
+        [Required(ErrorMessage = "El Título es Requerido")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "El Colegiado es Requerido")]
+        [StringLength(6, ErrorMessage = "Deben ser menos de 6 dígitos")]
+        public string Colegiado { get; set; }
+
+        public Catedratico()
+        {
+            Titulo = string.Empty;
+            Colegiado = string.Empty;
+        }
+    }
+}
